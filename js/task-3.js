e936e58081caba9203b4ffaa7bd79ddf265ba6f5
Artem Kulish <akulish247@gmail.com>
2025-05-07 20:35:49 +0200

    update

---
+++ js/task-3.js
@@ -0,0 +1,32 @@
+class StringBuilder {
+    // Приватна властивість
+    #value;
+
+    constructor(initialValue) {
+        this.#value = initialValue;
+    }
+// Повертає значення value
+    getValue() {
+        return this.#value;
+    }
+// Додає рядок в кінець 
+    padEnd(str) {
+        this.#value = this.#value + str;
+    }
+// Додає рядок на початок 
+    padStart(str) {
+        this.#value = str + this.#value;
+    }
+// Додає рядок на початок в кінець 
+    padBoth(str) {
+        this.#value = str + this.#value + str;
+    }
+}
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
